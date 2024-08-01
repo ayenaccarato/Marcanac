e7113ac788a02c7c2c24210817827f7f6360cd47
ayenaccarato <62022590+ayenaccarato@users.noreply.github.com>
2024-07-31 23:12:35 -0300

    Add files via upload

--- gui/usuario/usuario.py
+++ gui/usuario/usuario.py
@@ -1,3 +1,4 @@
+import os
 from PyQt6 import uic
 from PyQt6.QtWidgets import QMessageBox
 
@@ -8,7 +9,26 @@ from model.usuario import Usuario
 class UsuarioWindow():  
 
     def __init__(self):
-        self.nuevo = uic.loadUi("gui/usuario/nuevo_usuario.ui")
+        #self.nuevo = uic.loadUi("gui/usuario/nuevo_usuario.ui")
+        ui_file = os.path.join(os.path.dirname(__file__), '..', 'usuario', 'nuevo_usuario.ui')
+        ui_file = os.path.abspath(ui_file)  # Convierte a ruta absoluta
+        if not os.path.isfile(ui_file):
+            print(f"Error: el archivo {ui_file} no se encuentra.")
+            return
+        self.nuevo = uic.loadUi(ui_file)
+    
+    # def __init__(self):
+    #     try:
+    #         ui_file = os.path.join(os.path.dirname(__file__), '..', 'gui', 'usuario', 'nuevo_usuario.ui')
+    #         ui_file = os.path.abspath(ui_file)
+            
+    #         if not os.path.isfile(ui_file):
+    #             raise FileNotFoundError(f"El archivo {ui_file} no se encuentra.")
+            
+    #         self.nuevo = uic.loadUi(ui_file)
+    #         print("Archivo .ui cargado exitosamente.")
+    #     except Exception as e:
+    #         print(f"Error al cargar el archivo .ui: {e}")
 
     def abrirRegistro(self): 
         self.nuevo.btnRegistrar.clicked.connect(self.registrar)     
@@ -40,14 +60,15 @@ class UsuarioWindow():
 
             objData = UsuarioData()
                     
-            mBox = QMessageBox()
+            #mBox = QMessageBox()
             if objData.crear_usuario(usuario=nuevoUsuario):   
-                mBox.setWindowTitle('Mensaje')             
-                mBox.setText("Usuario registrado")      
+                # mBox.setWindowTitle('Mensaje')             
+                # mBox.setText("Usuario registrado")      
+                QMessageBox.information(None, 'Mensaje', 'Usuario registrado')
                                
             else:
-                mBox.setWindowTitle('Error')
-                mBox.setText("El usuario no pudo ser registrado")
-                        
-            mBox.exec()
+                #mBox.setWindowTitle('Error')
+                #mBox.setText("El usuario no pudo ser registrado")
+                QMessageBox.warning(None, 'Error', 'El usuario no pudo ser registrado')       
+            #mBox.exec()
             self.nuevo.close() #Cierro la ventana
