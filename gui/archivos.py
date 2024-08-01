9b03365ff5867b6f5c47670c000fd7868d4fc55b
ayenaccarato <62022590+ayenaccarato@users.noreply.github.com>
2024-07-31 23:13:01 -0300

    Add files via upload

--- gui/archivos.py
+++ gui/archivos.py
@@ -1,3 +1,4 @@
+import os
 from PyQt6 import uic
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QMessageBox
@@ -5,6 +6,12 @@ from PyQt6.QtWidgets import QMessageBox
 class ArchivosWindow():
 
     def __init__(self):
-        self.arc = uic.loadUi("gui/archivos.ui")
+        #self.arc = uic.loadUi("gui/archivos.ui")
+        ui_file = os.path.join(os.path.dirname(__file__), '..', 'gui', 'archivos.ui')
+        ui_file = os.path.abspath(ui_file)  # Convierte a ruta absoluta
+        if not os.path.isfile(ui_file):
+            print(f"Error: el archivo {ui_file} no se encuentra.")
+            return
+        self.arc = uic.loadUi(ui_file)
 
     
